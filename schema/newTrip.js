413d6fce5137d3270755ec1ba6fa8b24ba6a7a5c
Meyers <augustmeyers14@gmail.com>
2025-03-03 16:12:38 -0500

    Use basic Zod for querying, validate with heavier

--- schema/newTrip.js
+++ schema/newTrip.js
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 const textSchema = z.string().min(5).max(300);
 
-export const newTrip = z.object({
+const newTrip = z.object({
     id: textSchema,
     where: textSchema,
     when: textSchema,
     description: textSchema
 });
+
+export { newTrip }
