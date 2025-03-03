bb483959ffcae2b0cf165cd2571b542a81ba08d7
meyersa <88554476+meyersa@users.noreply.github.com>
2025-03-03 11:21:18 -0500

    Initial Refactor

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import NodeCache from "node-cache";
-import { validateTrip } from "./lib/validateTrip.js";
-import { populateImages } from "./lib/populateImages.js";
-import { getImage } from "./lib/handleImages.js";
+import { getImage } from "./lib/storage/images.js";
 import { configDotenv } from "dotenv";
-import { generate } from "./lib/queryOpenAI.js";
-import { initMongo } from "./lib/mongo.js";
+import { generate } from "./lib/openAI/queryOpenAI.js";
+import { initMongo } from "./lib/storage/mongo.js";
+import { preFlightLog, cleanAndVerify } from "./lib/util.js";
+import { getTrip, getTrips, getFail, populateFail, populateAndSubmit } from "./lib/storage/trips.js";
 configDotenv();
 
 const { MONGO_URL, MONGO_DB, SERVER_KEY, OPENAI_KEY } = process.env;
@@ -16,20 +16,11 @@ console.log("Starting server...");
 const app = express();
 const cache = new NodeCache();
 
-function defaultValidation() {
-  console.log("Validing ENVs");
-
-  if (!MONGO_URL || !MONGO_DB) {
-    throw new Error("Missing Mongo ENVs");
-  }
-
-  if (!SERVER_KEY) {
-    throw new Error("Missing Server API Key");
-  }
-
-  console.log("Validated default variables");
+console.log("Validing ENVs");
+if (!SERVER_KEY) {
+  throw new Error("Missing Server API Key");
 }
-defaultValidation();
+console.log("Validated default variables");
 
 // Setup Rate Limiter
 let lastExecutionTime = 0;
@@ -47,8 +38,6 @@ function checkRate() {
 }
 
 const db = await initMongo(MONGO_URL, MONGO_DB);
-const tripsDB = db.collection("trips");
-const failsDB = db.collection("fails");
 
 // Setup template engine, view (template) dir, and asset route
 app.set("view engine", "ejs");
@@ -82,41 +71,6 @@ async function tryCacheOrSet(name, valueFunction, cacheLength = 3600) {
   return newRes;
 }
 
-/*
- * Get a trip ID (Reuse tryCacheOrSet for this)
- * 1. Check cache for trip ID, return if exist
- * 2. Query Mongo for trip ID, throw error if not exist
- * 3. Set cache for trip ID
- * 4. Return trip
- */
-async function getTrip(id) {
-  return await tryCacheOrSet(id, async () => {
-    console.log(`Getting trip information for ${id}`);
-    if (!tripsDB) {
-      throw new Error("Mongo is unavailable");
-    }
-
-    return await tripsDB.findOne({
-      id: id,
-    });
-  });
-}
-
-// Basic log for information, can add more with more headers
-function preFlightLog(req) {
-  let output = `Received request on ${req.path}`;
-
-  if (req.query) {
-    output += `, with queries ${JSON.stringify(req.query)}`;
-  }
-
-  if (req.body.length > 1) {
-    output += `, and a body`;
-  }
-
-  console.log(output);
-}
-
 async function handleUnavailable(res, req) {
   console.log(`Received unavailable on ${req.path}. Redirecting...`);
   return res.redirect("/unavailable");
@@ -125,140 +79,6 @@ async function handleUnavailable(res, req) {
 async function handleNotFound(res, req) {
   console.log(`Received request on nonexistent page ${res.path}. Redirecting...`);
   return res.redirect("/notfound");
-}
-
-// Clean and verify an input
-function cleanAndVerify(value, minLength = 5, maxLength = 300) {
-  if (!value) {
-    throw new Error("Input value is invalid");
-  }
-
-  value = String(value).trim();
-  if (value.length < minLength) {
-    throw new Error("Input value is too short");
-  }
-  if (value.length > maxLength) {
-    throw new Error("input value is too long");
-  }
-
-  return value;
-}
-
-async function getTrips() {
-  console.log("Getting trips from Mongo");
-  if (!tripsDB) {
-    throw new Error("Mongo is unavailable");
-  }
-
-  return await tripsDB
-    .find(
-      {},
-      {
-        projection: {
-          id: 1,
-          name: 1,
-          startDate: 1,
-          description: 1,
-          picture: 1,
-          _id: 0,
-        },
-      }
-    )
-    .toArray();
-}
-
-/*
- * Handle incoming TripJSON
- * Assumed to be already filled in
- *
- * 1. Verifies Structure
- * 2. Checks for Duplicates
- * 3. Populates Images
- * 4. Inserts into Mongo
- * 5. Reset trip cache
- *
- * Everything is caught so output can be assumed safe to render to user if wanted
- */
-async function populateAndSubmit(tripJSON) {
-  // 1. Validate Trip
-  try {
-    await validateTrip(tripJSON);
-  } catch (err) {
-    console.error("Failed to validate tripJSON", err);
-    throw new Error("Failed to validate tripJSON");
-  }
-
-  // 2. Check for Dups
-  try {
-    console.log("Checking Mongo for duplicate trips...");
-    let result = await tripsDB.findOne({ id: tripJSON.id });
-
-    if (!!result) {
-      throw new Error();
-    }
-  } catch (err) {
-    console.error("Found a duplicate trip ID", err);
-    throw new Error("Trip name already exists");
-  }
-
-  // 3. Populate Images
-  try {
-    tripJSON = await populateImages(OPENAI_KEY, tripJSON, db);
-  } catch (err) {
-    console.error("Failed to populate images from google", err);
-    throw new Error("Failed to populate images from google");
-  }
-
-  // 4. Insert result if not dup
-  try {
-    console.log("Inserting trip into Mongo...");
-    const result = await tripsDB.insertOne(tripJSON);
-
-    if (!result.acknowledged) {
-      throw new Error("Result not acknowledged");
-    }
-
-    console.log("Uploaded trip to Mongo");
-  } catch (err) {
-    console.error("Failed to upload document to Mongo", err);
-    throw new Error("Failed to upload document to Mongo");
-  }
-
-  try {
-    cache.del("trips");
-  } catch (err) {
-    console.error("Failed to delete Trip cache", err);
-  }
-
-  console.log("Finished populating and submitting.");
-  return true;
-}
-
-// Handle failure submission
-async function populateFail(id) {
-  console.log(`Failured received when creating trip ${id}`);
-
-  try {
-    await failsDB.insertOne({ id: id });
-
-    console.log(`Uploaded failure to Mongo ${id}`);
-  } catch (err) {
-    console.error(`Failed to upload failure (ironic) to Mongo ${id}`);
-  }
-}
-
-// Check for fail
-async function getFail(id) {
-  return await tryCacheOrSet(id, async () => {
-    console.log(`Checking for fail ${id}`);
-    if (!failsDB) {
-      throw new Error("Mongo is unavailable");
-    }
-
-    return await failsDB.findOne({
-      id: id,
-    });
-  });
 }
 
 /*
